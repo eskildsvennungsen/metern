a5d390fcd684f1d693d27616df54fe599f8ae5aa
Eskild Dybwad Svennungsen <eskild@metern.no>
2024-06-22 00:12:04 +0200

    update api call with regards to changes

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,30 +3,29 @@ import React from 'react';
 const apiURI = 'http://localhost:4000';
 
 export const Input = (props) => {
+  const handleSubmit = async (e) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        // Prevent the browser from reloading the page
-        e.preventDefault();
-    
-        // Read the form data
-        const form = e.target;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-        
-        const res = await fetch(`${apiURI}/country/distance?from=` + formJson.newGuess + `&to=` + props.data.country.name).then((res) => res.json());
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    const guess = formJson.newGuess;
 
-        const arr = props.data.guesses.concat({"country": res.a, "distance": res.distance});
-        props.data.setGuesses(arr);       
-    }
+    const res = await fetch(`${apiURI}/country/check?target=` + guess).then((res) => res.json());
 
-    return(     
-        <form className='opacity-100' method="post" onSubmit={handleSubmit}>
-            <label>
-                <input className='text-black text-center' name="newGuess" />
-            </label>
-            <hr />
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
+    const arr = props.data.guesses.concat({ country: guess, distance: res.distance });
+    props.data.setGuesses(arr);
+  };
 
+  return (
+    <form className='opacity-100' method='post' onSubmit={handleSubmit}>
+      <label>
+        <input className='text-black text-center' name='newGuess' />
+      </label>
+      <hr />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+};
