ad39fce31b3d3a0b569e9f24afc511e34f7a587b
lopen1 <trygveulsnes@gmail.com>
2024-06-21 23:37:18 +0200

    Update to MyGlobe to make it highlight countries' polygon data when guessed

--- client/src/components/MyGlobe.jsx
+++ client/src/components/MyGlobe.jsx
@@ -1,28 +1,39 @@
 import Globe from 'react-globe.gl';
-import globeImage from '../assets/earth-day.jpg';
+import globeImage from '../assets/gray-earth.png'; //'../assets/earth-day.jpg';
 import { useEffect, useState, useRef } from 'react';
 
 export const MyGlobe = (props) => {
   const thisGlobe = useRef();
-  const [countries, setCountries] = useState({features: []});
+  const [countries, setCountries] = useState([]);
+  const [polyData, setPolyData] = useState([]);
   const [hoverD, setHoverD] = useState();
 
+  const drawPolygons = () => {
+    props.data.guesses.forEach(guess => {
+      const arr = polyData.concat(countries.find((country) => {
+                                  return country.properties.NAME_LONG === guess.country.name;
+                                  }));
+      setPolyData(arr);
+      return;
+    })
+  };
+
   useEffect(() => {
-    // load data
-    fetch('../../dataset/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(data => setCountries(data));
+    fetch('../../dataset/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(data => setCountries(data.features));
   }, []);
 
-  // will rotate the view to this point
-  //const MAP_CENTER = { lat: 37.6, lng: -16.6, altitude: 0.4 }; 
-  //thisGlobe.current.pointOfView(MAP_CENTER, 10000); 
+  useEffect(() => {
+    drawPolygons();
+  }, [props.data.guesses]);
 
   return (
     <Globe 
       ref={thisGlobe}
       globeImageUrl={globeImage}
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      backgroundColor='#fcfcff'
 
-      polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
+      polygonsData={polyData} //countries.features}
       polygonAltitude={d => d === hoverD ? 0.03 : 0.01}
       polygonCapColor={d => d === hoverD ? 'steelblue' : 'green'}
       polygonSideColor={() => 'rgba(0, 100, 0, 1)'}
