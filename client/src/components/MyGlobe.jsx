7fcf52aebaafda78f83e9504dbf3891004376946
Eskild Svennungsen <eskildsvennungsen@gmail.com>
2024-06-23 12:20:18 +0200

    initial color setup

--- client/src/components/MyGlobe.jsx
+++ client/src/components/MyGlobe.jsx
@@ -1,34 +1,75 @@
 import Globe from 'react-globe.gl';
-import globeImage from '../assets/earth-day.jpg';
+import globeImage from '../assets/plain.jpg';
 import { useEffect, useState, useRef } from 'react';
 
 export const MyGlobe = (props) => {
   const thisGlobe = useRef();
-  const [countries, setCountries] = useState({features: []});
+  const [countries, setCountries] = useState({ features: [] });
   const [hoverD, setHoverD] = useState();
+
+  async function assignColors() {
+    props.data.guesses.map((guess) => {
+      countries.features.map((feature) => {
+        if (feature.properties.NAME_LONG.toLowerCase() === guess.country.name.toLowerCase()) {
+          feature.properties.polygonCapColor = distanceColor(guess.distance);
+        }
+      });
+    });
+  }
+
+  function distanceColor(distance) {
+    const distanceCap = 13000; // KM, after 12k the color is just assigned red
+    if (distance > distanceCap) {
+      return 'rgba(150,0,0,1)';
+    }
+
+    if (distance === 0) {
+      return 'rgba(111,232,90,1)';
+    }
+
+    const turningPoint = distanceCap * 0.4;
+    const normalizedPreTP = (distance - turningPoint) / (distanceCap - turningPoint);
+    const normalizedPostTP = distance / turningPoint;
+    const coloBase = 150;
+    let redValue = 0;
+    let greenValue = 0;
+
+    if (distance > turningPoint) {
+      redValue = coloBase;
+      greenValue = Math.floor(coloBase * (1 - normalizedPreTP));
+    } else {
+      redValue = Math.floor(coloBase * normalizedPostTP);
+      greenValue = coloBase;
+    }
+
+    return `rgba(${redValue},${greenValue},0,1)`;
+  }
 
   useEffect(() => {
     // load data
-    fetch('../../dataset/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(data => setCountries(data));
+    fetch('../../dataset/ne_110m_admin_0_countries.geojson')
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
   }, []);
 
   // will rotate the view to this point
-  //const MAP_CENTER = { lat: 37.6, lng: -16.6, altitude: 0.4 }; 
-  //thisGlobe.current.pointOfView(MAP_CENTER, 10000); 
+  //const MAP_CENTER = { lat: 37.6, lng: -16.6, altitude: 0.4 };
+  //thisGlobe.current.pointOfView(MAP_CENTER, 10000);
+
+  assignColors();
 
   return (
-    <Globe 
+    <Globe
       ref={thisGlobe}
       globeImageUrl={globeImage}
-      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-
-      polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
-      polygonAltitude={d => d === hoverD ? 0.03 : 0.01}
-      polygonCapColor={d => d === hoverD ? 'steelblue' : 'green'}
-      polygonSideColor={() => 'rgba(0, 100, 0, 1)'}
+      backgroundImageUrl='//unpkg.com/three-globe/example/img/night-sky.png'
+      polygonsData={countries.features.filter((d) => d.properties.ISO_A2 !== 'AQ')}
+      polygonAltitude={(d) => (d === hoverD ? 0.03 : 0.01)}
+      polygonCapColor={(d) => d.properties.polygonCapColor || '#FED8B1'}
+      polygonSideColor={() => 'rgba(255, 255, 255, 1)'}
       polygonStrokeColor={() => '#111'}
       onPolygonHover={setHoverD}
       polygonsTransitionDuration={300}
     />
   );
-}
+};
