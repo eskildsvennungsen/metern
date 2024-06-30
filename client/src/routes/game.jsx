21705ad1dc20c644c743fb34ba2fa98193a7abfb
lopen1 <trygveulsnes@gmail.com>
2024-06-30 18:57:40 +0200

    no padding top for info/victory box

--- client/src/routes/game.jsx
+++ client/src/routes/game.jsx
@@ -28,13 +28,13 @@ const Game = () => {
       <div className='absolute top-0 right-0 w-auto max-h-100 z-50'>
         <Menu data={data}/>
       </div>
-      <div className='absolute top-5 w-2/3 sm:w-5/6 sm:max-w-xl z-50'>
+      <div className='absolute top-0 w-2/3 sm:w-5/6 sm:max-w-xl z-50'>
         {victory ? <VictoryBox data={data} /> : <InfoBox data={data} />}
       </div>
       <div className='w-fill bg-stone-950 absolute'>
         <MyGlobe data={data} />
       </div>
-      <div className='bottom-5 absolute w-2/3 sm:w-5/6 sm:max-w-xl'>
+      <div className='bottom-5 fixed w-2/3 sm:w-5/6 sm:max-w-xl'>
         {!victory && <Input data={data} />}
       </div>
     </div>
