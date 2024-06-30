cea83a1d815740b184a7e33259a84823e7abb922
lopen1 <trygveulsnes@gmail.com>
2024-06-30 19:24:20 +0200

    removed hammy for now

--- client/src/routes/game.jsx
+++ client/src/routes/game.jsx
@@ -25,9 +25,6 @@ const Game = () => {
 
   return (
     <div className='flex justify-center overflow-hidden select-none'>
-      <div className='absolute top-0 right-0 w-auto max-h-100 z-50'>
-        <Menu data={data}/>
-      </div>
       <div className='absolute top-0 w-2/3 sm:w-5/6 sm:max-w-xl z-50'>
         {victory ? <VictoryBox data={data} /> : <InfoBox data={data} />}
       </div>
