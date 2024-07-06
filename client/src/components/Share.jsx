5081e3e4f8f6b07571b2acbe8a37b04a5ec983d9
lopen1 <trygveulsnes@gmail.com>
2024-07-06 19:14:46 +0200

    add todays metern number to title of shareable message

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { constructShareable } from "../utilities/shareable";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { apiURI } from "../main";
 
 export const Share = (props) => {
     const [results, setResults] = useState();
     const [copied, setCopied] = useState(false);
 
+    const readyResults = async () => {
+        try {
+            await fetch(`${apiURI}/country/load`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const title = 'Metern #' + data.played + ' - ' + (props.stats.guesses + 1) + '/10\n\n';
+                        const shareable = constructShareable(props.data.guesses).map((item) => {
+                                                return item.square + item.direction;
+                                            }).join('\n');
+                        setResults(title + shareable);
+                    });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
-        const res = constructShareable(props.data.guesses).map((item) => {
-            return item.square + item.direction;
-        }).join('\n');
-
-        const msg = 'Metern #' + "XX" + ' - ' + (props.stats.guesses + 1) + '/10\n\n' + res;
-
-        setResults(msg);
+        readyResults();
     }, []); 
 
     return (
